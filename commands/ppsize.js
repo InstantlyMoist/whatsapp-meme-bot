b3624772ac5228e8ffb3ecba9dab76b0bb7e6680
InstantlyMoist <kyllian007@gmail.com>
2020-04-17 14:02:29 +0200

    Added ppsize command

--- commands/ppsize.js
+++ commands/ppsize.js
@@ -1,7 +1,8 @@
 exports.run = (client, msg, args) => {
-    let dankrate = getRandomInt(100);
-    msg.reply(`*dank r8 machine*\nYou are ${dankrate}% dank`);
-}
+    let ppLength = "=".repeat(getRandomInt(15)); 
+    msg.reply(`*peepee size machine*\nYour penis:\n8${ppLength}D`);
+
+};
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
